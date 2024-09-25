6ae6793c8a23aa48497ef1e18e0a296b591779b7
Divine <divinetidy@gmail.com>
2024-09-25 15:38:01 +0100

    updated

--- src/assets/icons/BlochainIcons.jsx
+++ src/assets/icons/BlochainIcons.jsx
@@ -1,9 +1,19 @@
-import styled from "styled-components";
+import styled , {keyframes} from "styled-components";
 import blockchainImages from "../Image/Vertical container.png"
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 
+`
 const Layout = styled.div`
   width: 342px;
   height: 223px;
+  animation: ${rotate} 10s linear infinite; /* Animation properties */
+
 `;
 const BlochainIcons = () => {
   return (
